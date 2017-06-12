6fd2ddd590ea5b08a45aed52f9a80a4437bbdfe5
박진수 <qkrwlstn1022@naver.com>
2017-06-13 02:16:10 +0900

    버전2
    SQL_member.spl 추가
    SQL_product.sql 추가
    상품 게시판? 기능 추가

---
+++ ShoppingMall/SQL_member.sql
@@ -0,0 +1,11 @@
+create table shopping_member(
+	name varchar2(30),
+	userid varchar2(30),
+	pwd varchar2(20),
+	gender number(1),
+	email varchar2(30),
+	phone char(13),
+	admin number(1)
+);
+
+select * from shopping_member;
